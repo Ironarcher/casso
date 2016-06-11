031928d9df42d5b1e1bedc1bff6ddb3cd0204d3c
Arpad Kovesdy <arpad.kovesdy@gmail.com>
2016-06-10 19:19:25 -0500

    Added POST method for example

--- casso/settings.py
+++ casso/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'casso_com'
 ]
 
 MIDDLEWARE_CLASSES = [
--- casso_com/views.py
+++ casso_com/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
+from django.http import *
+import time
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Casso has started.")
+	if request.POST:
+		username = request.POST['username']
+		time.sleep(2)
+		result = authenticate(username)
+	context = {
+		
+	}
+	return render(request, 'casso_com/example.html', context)
+
+def authenticate(username):
+	pass
