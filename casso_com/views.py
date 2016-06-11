efbcb999dda99be6f2ff63f81e7c41d20a334bee
Arpad Kovesdy <arpad.kovesdy@gmail.com>
2016-06-11 01:34:27 -0500

    Completed verification demo
    Minus the actual API connection to the casso server

--- casso_com/views.py
+++ casso_com/views.py
@@ -5,14 +5,28 @@ import time
 # Create your views here.
 
 def index(request):
-	if request.POST:
-		username = request.POST['username']
-		time.sleep(2)
-		result = authenticate(username)
-	context = {
-		
-	}
-	return render(request, 'casso_com/example.html', context)
+	result = False
+	if request.is_ajax():
+		if request.POST:
+			request_type = request.POST['type']
+			username = request.POST['username']
+			if request_type == "verify_username":
+				result = check_username(username)
+				return HttpResponse(result)
+			elif request_type == "verify_phone":
+				authenticate(username)
+				#Use HttpRedirect to success page
+	else:
+		context = {}
+		return render(request, 'casso_com/example.html', context)
 
+#Placeholder function (will be actual authentication by the web server)
+def check_username(username):
+	if(username == "big_arp"):
+		return True
+	else:
+		return False
+
+#To forward to casso api (to verify with phone)
 def authenticate(username):
 	pass
